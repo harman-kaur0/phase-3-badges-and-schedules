464cc4d553ecdb38cb5321abe19a673f3162e232
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-05-16 17:46:31 -0700

    commit tree 9ffdfeb15a9f80dc03027dc75c7ffb9746acca69

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|i| "Hello, my name is #{i}."}
+end
+
+
+def assign_rooms(speakers)
+    arr=[]
+    speakers.each.with_index(1) {|speaker, room| arr.push("Hello, #{speaker}! You'll be assigned to room #{room}!")}
+    arr
+
+end
+
+def printer(names)
+    batch_badge_creator(names).each {|msg| puts msg}
+    assign_rooms(names).each {|msg| puts msg}
+
+end
